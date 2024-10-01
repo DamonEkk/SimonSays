9656d00ffb4287c9a70a2cc117bffc0ba2b269e3
damon <damon.eccles@hotmail.com>
2024-10-01 22:58:25 +1000

    fix compile issue

--- SimonSays/src/main.c
+++ SimonSays/src/main.c
@@ -1,3 +1,7 @@
+#include "uart.h"
+#include "timer.h"
+#include "spi.h"
+
 #include <stdio.h>
 #include <stdint.h>
 
@@ -6,10 +10,10 @@ uint8_t sequence = 1;
 
 int main(void){ 
     // setup
-    
+    uart_init();
 
     while (1){  // Game loop
-
+        timer_init();
 
     }
 
